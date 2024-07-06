d08330057b1045ab0c7a492d0f30acff46902510
Jabez <jabezkassa2022@gmail.com>
2024-07-06 12:30:21 +0300

    updating pipeline

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -5,6 +5,8 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough, RunnableLambda
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain.load import dumps, loads
 import os
 from uuid import uuid4
 from langchain.prompts import ChatPromptTemplate
@@ -24,7 +26,6 @@ os.environ["LANGCHAIN_TRACING_V2"] = "true"
 unique_id = uuid4().hex[0:8]
 os.environ["LANGCHAIN_PROJECT"] = f"Contract Advisor RAG - {unique_id}"
 os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
-
 
 client = Client()
 
@@ -50,7 +51,7 @@ def simple_pipeline(vectorstore, question):
         """
     )
     
-    llm = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=1)
+    llm = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0.5)
 
     def format_docs(docs):
         return "\n\n".join(doc.page_content for doc in docs)
@@ -77,8 +78,6 @@ def multi_query_pipeline(vectorstore, question):
     Provide these alternative questions separated by newlines. Original question: {question}"""
     prompt_perspectives = ChatPromptTemplate.from_template(template)
 
-    from langchain_core.output_parsers import StrOutputParser
-    from langchain_openai import ChatOpenAI
 
     generate_queries = (
         prompt_perspectives 
@@ -109,7 +108,7 @@ def multi_query_pipeline(vectorstore, question):
 
     prompt = ChatPromptTemplate.from_template(template)
 
-    llm = ChatOpenAI(temperature=0)
+    llm = ChatOpenAI(model="gpt-4",temperature=0.8)
 
     final_rag_chain = (
         {"context": retrieval_chain, 
@@ -121,3 +120,68 @@ def multi_query_pipeline(vectorstore, question):
 
     answer = final_rag_chain.invoke({"question":question})
     return answer
+
+def rag_fusion(vectorstore, question):
+    llm = ChatOpenAI(model="gpt-4",temperature=0.8)
+    retriever = vectorstore.as_retriever()
+    # RAG-Fusion: Related
+    template = """You are a helpful assistant that generates multiple search queries based on a single input query. \n
+    Generate multiple search queries related to: {question} \n
+    Output (4 queries):"""
+    prompt_rag_fusion = ChatPromptTemplate.from_template(template)
+
+    generate_queries = (
+    prompt_rag_fusion 
+    | ChatOpenAI(temperature=0)
+    | StrOutputParser() 
+    | (lambda x: x.split("\n"))
+    )
+    def reciprocal_rank_fusion(results: list[list], k=60):
+        """ Reciprocal_rank_fusion that takes multiple lists of ranked documents and an optional parameter k used in the RRF formula """
+        
+        # Initialize a dictionary to hold fused scores for each unique document
+        fused_scores = {}
+
+        # Iterate through each list of ranked documents
+        for docs in results:
+            # Iterate through each document in the list, with its rank (position in the list)
+            for rank, doc in enumerate(docs):
+                # Convert the document to a string format to use as a key (assumes documents can be serialized to JSON)
+                doc_str = dumps(doc)
+                # If the document is not yet in the fused_scores dictionary, add it with an initial score of 0
+                if doc_str not in fused_scores:
+                    fused_scores[doc_str] = 0
+                # Retrieve the current score of the document, if any
+                previous_score = fused_scores[doc_str]
+                # Update the score of the document using the RRF formula: 1 / (rank + k)
+                fused_scores[doc_str] += 1 / (rank + k)
+
+        # Sort the documents based on their fused scores in descending order to get the final reranked results
+        reranked_results = [
+            (loads(doc), score)
+            for doc, score in sorted(fused_scores.items(), key=lambda x: x[1], reverse=True)
+        ]
+        return reranked_results
+    retrieval_chain_rag_fusion = generate_queries | retriever.map() | reciprocal_rank_fusion
+
+    # RAG
+    template = """Answer the following question based on this context:
+
+    {context}
+
+    Question: {question}
+    """
+
+    prompt = ChatPromptTemplate.from_template(template)
+
+    final_rag_chain = (
+        {"context": retrieval_chain_rag_fusion, 
+        "question": itemgetter("question")} 
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    answer = final_rag_chain.invoke({"question":question})
+    return answer
+
